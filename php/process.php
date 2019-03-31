1e804dd46ffe2a6c05688d359b6282bfe85ff2a9
McNuggets7778 <mockvich88@gmail.com>
2019-03-31 18:04:21 +0300

    site

--- php/process.php
+++ php/process.php
@@ -1,6 +1,6 @@
 <?php 
         $db = mysql_connect('localhost', 'root', '') or die ('error');
-        mysql_select_db('testshpt') or die('error');
+        mysql_select_db('mybasetest') or die('error');
           if(isset($_POST['submit'])){
             if(empty($_POST['name'])){}
             if(empty($_POST['email'])){}
@@ -8,10 +8,9 @@
 
               else{
               
-               mysql_query("INSERT INTO `shptmess` (`name`, `email`, `mess`) VALUES ('".htmlspecialchars($_POST['name'])."', '".htmlspecialchars($_POST['email'])."', '".htmlspecialchars($_POST['mess'])."');");
+               mysql_query("INSERT INTO `mytabletest` (`name`, `email`, `mess`) VALUES ('".htmlspecialchars($_POST['name'])."', '".htmlspecialchars($_POST['email'])."', '".htmlspecialchars($_POST['mess'])."');");
                 mysql_query('location :' .$_SERVER['PHP_SELF']);
               }
-              header('Location: ' . $_SERVER['HTTP_REFERER']);
           }
           mysql_close($db);
          ?>
